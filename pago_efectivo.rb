04e96527110fc3438389d4c7f24b6efd6e5bc315
Cesar Carruitero <cesar@mozilla.pe>
2014-07-18 12:08:55 -0500

    init pago efectivo implementation for ruby

---
+++ pago_efectivo.rb
@@ -0,0 +1,46 @@
+require 'gyoku'
+require 'builder'
+require 'net/http'
+
+module PagoEfectivo
+
+  SCHEMA_TYPES = [
+    {'xmlns:xsd' => 'http://www.w3.org/2001/XMLSchema'},
+    {'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance'},
+    {'xmlns:soap' => 'http://schemas.xmlsoap.org/soap/envelope/'}
+  ]
+
+  def initialize
+    @api_server = 'https://pre.pagoefectivo.pe'
+  end
+
+  def create_markup(body)
+    xml_markup = Builder::XmlMarkup.new(indent: 2)
+    xml_markup.instruct! :xml
+    xml_markup << body.to_s
+    xml_markup
+  end
+
+  def request_signature(text, private_key)
+    path = '/PagoEfectivoWSCrypto/WSCrypto.asmx'
+    hash = { signer: { plain_text: text, private_key: private_key }}
+    options = { key_converter: :camelcase, key_to_convert: 'signer'}
+    xml_body = Gyoku.xml(hash, options)
+
+    xml = create_markup(xml_header, xml_body)
+    puts xml.to_s
+    #request = Net::HTTP::Post.new(@api_server + path)
+  end
+
+  def generate_cip
+  end
+
+  def consult_cip
+  end
+
+  def delete_cip
+  end
+
+  def update_cip
+  end
+end
