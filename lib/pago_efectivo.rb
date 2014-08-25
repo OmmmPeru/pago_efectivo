44e4058e9bb1864a7d0a323810f792b58a133db3
Cesar Carruitero <cesar@mozilla.pe>
2014-08-25 17:55:09 -0500

    complete request cip

--- lib/pago_efectivo.rb
+++ lib/pago_efectivo.rb
@@ -52,36 +52,36 @@ module PagoEfectivo
     response.encrypt_text_result
   end
 
-  def generate_cip(signer, currency, total, pay_methods, cod_trans, email,
-                   user, additional_data)
-    # Cóidgo de servicio asignado
-    cServ = 'MEEH'
-    cClave = signer
+  def request_cip(cod_serv, signer, currency, total, pay_methods, cod_trans,
+                  email, user, additional_d, cod_servata, exp_date, place,
+                  pay_concept, origin_code, origin_type)
+    # cod_serv => código de servicio asignado
+    # signer => trama firmada con llave privada
     hash = { sol_pago: {
                id_moneda: currency.id,
-               total: total,
+               total: total, # 18 enteros, 2 decimales. Separados por `,`
                metodos_pago: pay_methods,
                cod_servicio: cServ,
-               cod_transaccion: cod_trans,
+               cod_transaccion: cod_trans, # referencia al pago
                email_comercio: email,
-               fecha_a_expirar: (DateTime.now + 4).to_s(:db),
+               fecha_a_expirar: exp_date, # (DateTime.now + 4).to_s(:db)
                usuario_id: user.id,
                data_adicional: additional_data,
                usuario_nombre: user.first_name,
                usuario_apellidos: user.last_name,
-               usuario_localidad: ,
-               usuario_provincia: ,
-               usuario_pais: ,
-               usuario_alias: ,
-               usuario_tipo_doc: ,
-               usuario_numero_doc: ,
-               usuario_email: ,
-               concepto_pago: ,
+               usuario_localidad: place.loc,
+               usuario_provincia: place.prov,
+               usuario_pais: place.country,
+               usuario_alias: '',
+               usuario_tipo_doc: user.doc_type, # tipo de documento DNI, LE, RUC
+               usuario_numero_doc: user.doc_num,
+               usuario_email: '',
+               concepto_pago: pay_concept,
                detalles: {
                  detalle: {
-                   cod_origen: ,
-                   tipo_origen: ,
-                   concepto_pago: ,
+                   cod_origen: origin_code,
+                   tipo_origen: origin_type,
+                   concepto_pago: pay_concept,
                    importe: total,
                    campo1: '',
                    campo2: '',
@@ -91,7 +91,7 @@ module PagoEfectivo
                params_url: {
                  param_url: {
                    nombre: 'IDCliente',
-                   valor:
+                   valor: user.id
                  },
                  param_url: {
                    nombre: 'FechaHoraRegistro',
@@ -100,7 +100,7 @@ module PagoEfectivo
                  params_email: {
                    param_email: {
                      nombre: '[UsuarioNombre]'
-                     valor: user.first_name
+                     valor: user.name
                    },
                    param_email: {
                      nombre: '[Moneda]',
@@ -110,6 +110,22 @@ module PagoEfectivo
                }
              }
            }
+    child_options = { key_converter: :camelcase}
+    xml_child = create_markup(Gyoku.xml(child_hash, child_options))
+
+    hash_parent = {generar_cip_mod_1: { request: {
+                    cod_serv: cod_serv,
+                    firma: signer,
+                    xml: xml_child
+                  }}}
+    attributes = {"soap:Envelope" => SCHEMA_TYPES}
+    options = { key_converter: :camelcase, except: 'request'}
+    xml_parent = Gyoku.xml({"soap:Envelope" => {"soap:Body" => hash_parent},
+                          :attributes! => attributes}, options)
+    xml = create_markup(xml_parent)
+    path = '/PagoEfectivoWSGeneralv2/service.asmx'
+    server = @api_server + path
+    response = @request.new(server, verify_ssl: true).post(xml)
   end
 
   def consult_cip
