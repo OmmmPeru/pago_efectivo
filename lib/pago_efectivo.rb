51eaab3450c3d6c6f6fd91b0f1629d967c2eb9f4
Cesar Carruitero <cesar@mozilla.pe>
2014-08-26 11:47:39 -0500

    move methods inside class

--- lib/pago_efectivo.rb
+++ lib/pago_efectivo.rb
@@ -5,181 +5,183 @@ require 'ox'
 
 module PagoEfectivo
 
-  SCHEMA_TYPES = {
-    'xmlns:xsd' => 'http://www.w3.org/2001/XMLSchema',
-    'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
-    'xmlns:soap' => 'http://schemas.xmlsoap.org/soap/envelope/'
-  }
+  class Client
+    SCHEMA_TYPES = {
+      'xmlns:xsd' => 'http://www.w3.org/2001/XMLSchema',
+      'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
+      'xmlns:soap' => 'http://schemas.xmlsoap.org/soap/envelope/'
+    }
 
-  def initialize env=nil
-    if env == 'production'
-      @api_server = 'https://pagoefectivo.pe'
-    else
-      @api_server = 'https://pre.pagoefectivo.pe'
+    def initialize env=nil
+      if env == 'production'
+        @api_server = 'https://pagoefectivo.pe'
+      else
+        @api_server = 'https://pre.pagoefectivo.pe'
+      end
+      @request = RestClient::Resource
     end
-    @request = RestClient::Resource
-  end
 
-  def create_markup(body)
-    xml_markup = Builder::XmlMarkup.new(indent: 2)
-    xml_markup.instruct! :xml
-    xml_markup << body.to_s
-    xml_markup
-  end
+    def create_markup(body)
+      xml_markup = Builder::XmlMarkup.new(indent: 2)
+      xml_markup.instruct! :xml
+      xml_markup << body.to_s
+      xml_markup
+    end
 
-  def signature(text, private_key)
-    path = '/PagoEfectivoWSCrypto/WSCrypto.asmx'
-    hash = { signer: { plain_text: text, private_key: private_key }}
-    options = { key_converter: :camelcase, key_to_convert: 'signer'}
-    attributes = {"soap:Envelope" => SCHEMA_TYPES}
-    xml_body = Gyoku.xml({"soap:Envelope" => {"soap:Body" => hash},
-                          :attributes! => attributes}, options)
+    def signature(text, private_key)
+      path = '/PagoEfectivoWSCrypto/WSCrypto.asmx'
+      hash = { signer: { plain_text: text, private_key: private_key }}
+      options = { key_converter: :camelcase, key_to_convert: 'signer'}
+      attributes = {"soap:Envelope" => SCHEMA_TYPES}
+      xml_body = Gyoku.xml({"soap:Envelope" => {"soap:Body" => hash},
+                            :attributes! => attributes}, options)
 
-    xml = create_markup(xml_body)
+      xml = create_markup(xml_body)
 
-    server = @api_server + path
-    response = Ox.parse(@request.new(server, verify_ssl: true).post(xml))
-    response.signer_result
-  end
+      server = @api_server + path
+      response = Ox.parse(@request.new(server, verify_ssl: true).post(xml))
+      response.signer_result
+    end
 
-  def encrypt_text(text, public_key)
-    path = '/PagoEfectivoWSCrypto/WSCrypto.asmx'
-    hash = { encrypt_text: { plain_text: text, public_key: public_key }}
-    options = { key_converter: :camelcase, key_to_convert: 'encrypt_text'}
-    attributes = {"soap:Envelope" => SCHEMA_TYPES}
-    xml_body = Gyoku.xml({"soap:Envelope" => {"soap:Body" => hash},
-                          :attributes! => attributes}, options)
+    def encrypt_text(text, public_key)
+      path = '/PagoEfectivoWSCrypto/WSCrypto.asmx'
+      hash = { encrypt_text: { plain_text: text, public_key: public_key }}
+      options = { key_converter: :camelcase, key_to_convert: 'encrypt_text'}
+      attributes = {"soap:Envelope" => SCHEMA_TYPES}
+      xml_body = Gyoku.xml({"soap:Envelope" => {"soap:Body" => hash},
+                            :attributes! => attributes}, options)
 
-    xml = create_markup(xml_body)
-    server = @api_server + path
-    response = Ox.parse(@request.new(server, verify_ssl: true).post(xml))
-    response.encrypt_text_result
-  end
+      xml = create_markup(xml_body)
+      server = @api_server + path
+      response = Ox.parse(@request.new(server, verify_ssl: true).post(xml))
+      response.encrypt_text_result
+    end
 
-  def request_cip(cod_serv, signer, currency, total, pay_methods, cod_trans,
-                  email, user, additional_d, cod_servata, exp_date, place,
-                  pay_concept, origin_code, origin_type)
-    # cod_serv => código de servicio asignado
-    # signer => trama firmada con llave privada
-    hash = { sol_pago: {
-               id_moneda: currency.id,
-               total: total, # 18 enteros, 2 decimales. Separados por `,`
-               metodos_pago: pay_methods,
-               cod_servicio: cServ,
-               cod_transaccion: cod_trans, # referencia al pago
-               email_comercio: email,
-               fecha_a_expirar: exp_date, # (DateTime.now + 4).to_s(:db)
-               usuario_id: user.id,
-               data_adicional: additional_data,
-               usuario_nombre: user.first_name,
-               usuario_apellidos: user.last_name,
-               usuario_localidad: place.loc,
-               usuario_provincia: place.prov,
-               usuario_pais: place.country,
-               usuario_alias: '',
-               usuario_tipo_doc: user.doc_type, # tipo de documento DNI, LE, RUC
-               usuario_numero_doc: user.doc_num,
-               usuario_email: '',
-               concepto_pago: pay_concept,
-               detalles: {
-                 detalle: {
-                   cod_origen: origin_code,
-                   tipo_origen: origin_type,
-                   concepto_pago: pay_concept,
-                   importe: total,
-                   campo1: '',
-                   campo2: '',
-                   campo3: '',
-                 }
-               },
-               params_url: {
-                 param_url: {
-                   nombre: 'IDCliente',
-                   valor: user.id
+    def request_cip(cod_serv, signer, currency, total, pay_methods, cod_trans,
+                    email, user, additional_d, cod_servata, exp_date, place,
+                    pay_concept, origin_code, origin_type)
+      # cod_serv => código de servicio asignado
+      # signer => trama firmada con llave privada
+      hash = { sol_pago: {
+                 id_moneda: currency.id,
+                 total: total, # 18 enteros, 2 decimales. Separados por `,`
+                 metodos_pago: pay_methods,
+                 cod_servicio: cServ,
+                 cod_transaccion: cod_trans, # referencia al pago
+                 email_comercio: email,
+                 fecha_a_expirar: exp_date, # (DateTime.now + 4).to_s(:db)
+                 usuario_id: user.id,
+                 data_adicional: additional_data,
+                 usuario_nombre: user.first_name,
+                 usuario_apellidos: user.last_name,
+                 usuario_localidad: place.loc,
+                 usuario_provincia: place.prov,
+                 usuario_pais: place.country,
+                 usuario_alias: '',
+                 usuario_tipo_doc: user.doc_type, # tipo de documento DNI, LE, RUC
+                 usuario_numero_doc: user.doc_num,
+                 usuario_email: '',
+                 concepto_pago: pay_concept,
+                 detalles: {
+                   detalle: {
+                     cod_origen: origin_code,
+                     tipo_origen: origin_type,
+                     concepto_pago: pay_concept,
+                     importe: total,
+                     campo1: '',
+                     campo2: '',
+                     campo3: '',
+                   }
                  },
-                 param_url: {
-                   nombre: 'FechaHoraRegistro',
-                   valor: DateTime.now.to_s(:db)
-                 },
-                 params_email: {
-                   param_email: {
-                     nombre: '[UsuarioNombre]',
-                     valor: user.name
+                 params_url: {
+                   param_url: {
+                     nombre: 'IDCliente',
+                     valor: user.id
                    },
-                   param_email: {
-                     nombre: '[Moneda]',
-                     valor: currency.symbol
+                   param_url: {
+                     nombre: 'FechaHoraRegistro',
+                     valor: DateTime.now.to_s(:db)
+                   },
+                   params_email: {
+                     param_email: {
+                       nombre: '[UsuarioNombre]',
+                       valor: user.name
+                     },
+                     param_email: {
+                       nombre: '[Moneda]',
+                       valor: currency.symbol
+                     }
                    }
                  }
                }
              }
-           }
-    child_options = { key_converter: :camelcase}
-    xml_child = create_markup(Gyoku.xml(child_hash, child_options))
+      child_options = { key_converter: :camelcase}
+      xml_child = create_markup(Gyoku.xml(child_hash, child_options))
 
-    hash_parent = {generar_cip_mod_1: { request: {
-                    cod_serv: cod_serv,
-                    firma: signer,
-                    xml: xml_child
-                  }}}
-    attributes = {"soap:Envelope" => SCHEMA_TYPES}
-    options = { key_converter: :camelcase, except: 'request'}
-    xml_parent = Gyoku.xml({"soap:Envelope" => {"soap:Body" => hash_parent},
-                          :attributes! => attributes}, options)
-    xml = create_markup(xml_parent)
-    path = '/PagoEfectivoWSGeneralv2/service.asmx'
-    server = @api_server + path
-    response = @request.new(server, verify_ssl: true).post(xml)
-  end
+      hash_parent = {generar_cip_mod_1: { request: {
+                      cod_serv: cod_serv,
+                      firma: signer,
+                      xml: xml_child
+                    }}}
+      attributes = {"soap:Envelope" => SCHEMA_TYPES}
+      options = { key_converter: :camelcase, except: 'request'}
+      xml_parent = Gyoku.xml({"soap:Envelope" => {"soap:Body" => hash_parent},
+                            :attributes! => attributes}, options)
+      xml = create_markup(xml_parent)
+      path = '/PagoEfectivoWSGeneralv2/service.asmx'
+      server = @api_server + path
+      response = @request.new(server, verify_ssl: true).post(xml)
+    end
 
-  def unencrypt enc_text, private_key
-    path = '/PagoEfectivoWSCrypto/WSCrypto.asmx'
-    hash = { decrypt_text: { encrupt_text: enc_text, private_key: private_key }}
-    options = { key_converter: :camelcase, key_to_convert: 'decrypt_text'}
-    attributes = {"soap:Envelope" => SCHEMA_TYPES}
-    xml_body = Gyoku.xml({"soap:Envelope" => {"soap:Body" => hash},
-                          :attributes! => attributes}, options)
+    def unencrypt enc_text, private_key
+      path = '/PagoEfectivoWSCrypto/WSCrypto.asmx'
+      hash = { decrypt_text: { encrupt_text: enc_text, private_key: private_key }}
+      options = { key_converter: :camelcase, key_to_convert: 'decrypt_text'}
+      attributes = {"soap:Envelope" => SCHEMA_TYPES}
+      xml_body = Gyoku.xml({"soap:Envelope" => {"soap:Body" => hash},
+                            :attributes! => attributes}, options)
 
-    xml = create_markup(xml_body)
+      xml = create_markup(xml_body)
 
-    server = @api_server + path
-    response = Ox.parse(@request.new(server, verify_ssl: true).post(xml))
-    response.decrypt_text_response
-  end
+      server = @api_server + path
+      response = Ox.parse(@request.new(server, verify_ssl: true).post(xml))
+      response.decrypt_text_response
+    end
 
-  def generate_cip token
-    server = '/GenPago.aspx'
-    query_str = '?Token=' + token.to_s
-    url = server + query_str
-    response = @request.get(url)
-  end
+    def generate_cip token
+      server = '/GenPago.aspx'
+      query_str = '?Token=' + token.to_s
+      url = server + query_str
+      response = @request.get(url)
+    end
 
-  def consult_cip capi, key, cips, info_request=nil
-    path = '/PagoEfectivoWSGeneral/WSCIP.asmx'
-    hash = { consultar_cip: {request: {
-               CAPI: capi, c_clave: key,
-               CIPS: {
-                 cips.each do |cip|
-                   string: cip
-                 end
-               },
-              info_request: info_request
-            }}}
-    options = { key_converter: :camelcase}
-    attributes = {"soap:Envelope" => SCHEMA_TYPES}
-    xml_body = Gyoku.xml({"soap:Envelope" => {"soap:Body" => hash},
-                          :attributes! => attributes}, options)
+    def consult_cip capi, key, cips, info_request=nil
+      path = '/PagoEfectivoWSGeneral/WSCIP.asmx'
+      cip_arr = {}
+      cips.each do |cip|
+        cip_arr = cip_arr.merge({string: cip})
+      end
+      hash = { consultar_cip: {request: {
+                 CAPI: capi, c_clave: key,
+                 CIPS: cip_arr,
+                info_request: info_request
+              }}}
+      options = { key_converter: :camelcase}
+      attributes = {"soap:Envelope" => SCHEMA_TYPES}
+      xml_body = Gyoku.xml({"soap:Envelope" => {"soap:Body" => hash},
+                            :attributes! => attributes}, options)
 
-    xml = create_markup(xml_body)
+      xml = create_markup(xml_body)
 
-    server = @api_server + path
-    response = Ox.parse(@request.new(server, verify_ssl: true).post(xml))
-    response.decrypt_text_response
-  end
+      server = @api_server + path
+      response = Ox.parse(@request.new(server, verify_ssl: true).post(xml))
+      response.decrypt_text_response
+    end
 
-  def delete_cip
-  end
+    def delete_cip
+    end
 
-  def update_cip
+    def update_cip
+    end
   end
 end
--- test/app_test.rb
+++ test/app_test.rb
@@ -2,9 +2,23 @@ require_relative 'helper'
 include PagoEfectivo
 
 scope do
-  test 'test' do
-    req = PagoEfectivo.request_signature('text', 'key')
-    assert req.include?('?xml')
-    assert req.include?('xmlns:soap')
+  test 'should set dev api_server when initialize without option' do
+    client = PagoEfectivo::Client.new
+    api_server = client.instance_variable_get(:@api_server)
+    assert api_server == 'https://pre.pagoefectivo.pe'
+  end
+
+  test 'should set prod api_server when initialize if you set option' do
+    client = PagoEfectivo::Client.new('production')
+    api_server = client.instance_variable_get(:@api_server)
+    assert api_server == 'https://pagoefectivo.pe'
+  end
+
+  test 'create_markup generate valid xml' do
+    client = PagoEfectivo::Client.new
+    xml = Gyoku.xml({key: 'value'})
+    markup = client.create_markup(xml)
+    assert markup.include?('?xml')
+    assert markup.include?('xmlns:soap')
   end
 end
